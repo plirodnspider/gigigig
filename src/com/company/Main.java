e053b2eff02808c8877d3f6a81993ea24a037beb
Miłosz 60307 <gdx151202@student.wsb.gda.pl>
2021-11-09 10:36:55 +0100

    wersja 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
         public static void main(String[] args) {
             System.out.println( "Hellow word");
+
+
+            System.out.println("Zioło to siurak");
         }
 
 }
